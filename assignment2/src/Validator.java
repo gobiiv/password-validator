a47a30227c1bd54bf8ef7b0eebe1a44ab856fdbf
gobiiv <39447197+gobiiv@users.noreply.github.com>
2018-06-09 18:42:05 -0300

    Stage 2
    this is stage 2 of assignment2.

--- assignment2/src/Validator.java
+++ assignment2/src/Validator.java
@@ -1,27 +1,74 @@
 public class Validator{
-	//fields
+	// fields
 
-	//constructor method
+	// constructor method
 	public Validator(){
 	}
 
-	//validate method to check if string is a sufficiently storng password
+	// validate method to check if string is a sufficiently strong password
 	public int validate(String s){
-		int pass = 0;
-
-		//This is the series of if statements to apply an error message.
-
-		//rule 1: check if the password is not listed as 'password'
+		
+		// This returns the sum of successful rules
+		return (validateRule1(s)
+				+ validateRule2(s)
+				+ validateRule3(s)
+				+ validateRule4(s)
+				+ validateRule5(s));
+	}
+	
+	public int validateRule1(String s){
+		// rule 1: check if the password is not listed as 'password'
+		// case insensitive
 		if (! ( s.equalsIgnoreCase("password") ) ){ 
-			pass ++;
+			return 1; //success
+		} else {
+			return 0; //fail
 		}
-
-		//rule 2: check if the password is atleast 8 charcters long.
-		if (s.length() > 7) {
-			pass ++;
-		}
-
-		return pass;
 	}
 
+	public int validateRule2(String s){
+		// rule 2: check if the password is at least 8 characters long
+		if (s.length() > 7) {
+			return 1; //success
+		} else {
+			return 0; //fail
+		}
+	}		
+		
+	public int validateRule3(String s){
+		// rule 3: check if the password contains a number
+		// 0 (char) = 48, 9 (char) = 57 
+		for (char c: s.toCharArray()){
+			if (c >= 48 && c <= 57){
+				return 1; //success
+			}
+		}
+		return 0; //fail
+	}			
+		
+	public int validateRule4(String s){
+		// rule 4: check if the password contains an upper case character
+		// A (char) = 65, Z (char) = 90 
+		for (char c: s.toCharArray()){
+			if (c >= 65 && c <= 90){
+				return 1; //success
+			}
+		}		
+		return 0; //fail
+	}			
+				
+	public int validateRule5(String s){
+		// rule 5: check if the password is less than 15 characters
+		if (s.length() < 16) {
+			return 1; //success
+		} else {
+			return 0; //fail
+		}
+	}
+		
+		
+		
+		
+		
+		
 }
